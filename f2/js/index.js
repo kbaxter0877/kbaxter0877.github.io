4a367c08ab201bcebd7692da32891a751ca3bf99
kbaxter0877 <kbaxter@sjnma.org>
2018-12-10 08:44:18 -0600

    not work

--- f2/js/index.js
+++ f2/js/index.js
@@ -2,11 +2,11 @@
 function annoy(){
     var fname =prompt('tell me your name');
 
-alert(fname + "are we there yet?");
+alert(fname + " are we there yet?");
 
-alert(fname + "are we there yet?");
+alert(fname + " are we there yet?");
 
-alert(fname + "are we there yet?");
+alert(fname + " are we there yet?");
 
 }
 
