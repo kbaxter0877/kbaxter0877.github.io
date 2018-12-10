64f769cf6e814c4a9387a009ff4136f22fee21b7
kbaxter0877 <kbaxter@sjnma.org>
2018-12-10 08:46:36 -0600

    fichkb

--- f2/js/index.js
+++ f2/js/index.js
@@ -2,6 +2,8 @@
 function annoy(){
     var fname =prompt('tell me your name');
 
+    document.getElementById("annoy").innerHTML = "I am annoyed"
+
 alert(fname + " are we there yet?");
 
 alert(fname + " are we there yet?");
