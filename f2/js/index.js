002746a1be87a78a43e4178ae1e2dd6b3a72ba15
kbaxter0877 <kbaxter@sjnma.org>
2018-12-11 08:42:00 -0600

    big save

--- f2/js/index.js
+++ f2/js/index.js
@@ -3,13 +3,10 @@ function annoy(){
     alert("runnings")
     var fname = prompt('tell me your name');
 
-    document.getElementById("annoy").innerHTML = "I am annoyed"
-
-while(true){
-
-alert(fname + " are we there yet?");
-
+    document.getElementById("annoy").innerHTML = "I am annoyed";
 }
+
+
 function mark_present()
 {
     var cadet_name = $('#cadet_name').val();
