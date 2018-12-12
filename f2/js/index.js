eb7da0e50283bd58e5436a57bc874d1220bf8e0c
kbaxter0877 <kbaxter@sjnma.org>
2018-12-12 08:18:49 -0600

    fast

--- f2/js/index.js
+++ f2/js/index.js
@@ -9,8 +9,11 @@ function annoy(){
 
 function mark_present()
 {
+    alert("running");
     var cadet_name = $('#cadet_name').val();
     alert(cadet_name+ "is here!");
+    $('#present_cadets').append("<ul id = 'cadets_list'></ul>");
+    $('#cadets_list').append("<li>"+cadet_name+"</lu>");
 }
 
 
