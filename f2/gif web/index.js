f369c74e652811edce3e87a6c267a30a1bd0f690
kbaxter0877 <kbaxter@sjnma.org>
2019-01-18 10:16:16 -0600

    save

--- f2/gif web/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var flag = false;
-    setInterval(function() {
-    flag = !flag;
-    $("body").css("background", flag ? "red" : "green");
-    $("body").css("background", flag ? "green" : "red");
-    }, 1000);
